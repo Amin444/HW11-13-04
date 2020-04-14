dd3887f9cb0750c5e93f6f0182f9f63d82b1ecfa
unknown <aminmirzoev123@gmail.com>
2020-04-14 07:45:38 +0500

    Table

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace HW11_13_04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Table table=new Table();
+        
+        }
+    }
+    class Table
+    {
+        public string LastName{get;set;}
+        public string FirstName{get;set;}
+        public int Age{get;set;}
+        public int TelNum{get;set;}
+        public string gmail{get;set;}
+    }
+}
